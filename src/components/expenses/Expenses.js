5ebdf63d1c3f426b311ef9c264ebaf9d709ac3e8
filip.trajkovski <filip.trajkovski@sanitas.com>
2023-08-29 16:28:03 +0200

    Adding UI conditionally.

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpenseItem";
 import React, {useState} from "react";
-import './ExpenseList.css';
 import Card from "../UI/Card";
+import './Expense.css'
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 export default Expenses;
 
@@ -31,21 +31,6 @@ function Expenses(props) {
         setSelectedYear(year);
     }
 
-    let expensesContent = <p>No expenses found</p>;
-
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map(expense => {
-            return (
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-            );
-        })
-    }
-
     return (
         <div>
             <Card className="expenses">
@@ -53,7 +38,7 @@ function Expenses(props) {
                     chosenYear={selectedYear}
                     onSelectedYear={selectYear}/>
                 <p>{filterInfoText}</p>
-                {expensesContent}
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
     )
